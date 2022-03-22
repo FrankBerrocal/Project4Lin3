8304fbfb67b279ed6be70fd6da00fdb1d299acde
Peter <p.kosa089@mybvc.ca>
2022-03-22 16:14:10 -0600

    initial commit to create project files

---
+++ ConnectFourGame/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectFourGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "*** CONNECT 4 GAME *** by Peter Kosa & Frank Berrocal";
+            Console.ReadLine();
+        }
+    }
+}
