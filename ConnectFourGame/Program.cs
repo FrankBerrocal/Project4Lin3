c3edb92f8a5b3f48c3a95bdc572355e3e2a594a7
FrankBerrocal <Francisco.berrocal@me.com>
2022-04-06 14:11:28 -0600

    Accepting Windows Files
    Configuration from Peter's Windows

--- ConnectFourGame/Program.cs
+++ ConnectFourGame/Program.cs
@@ -7,6 +7,8 @@ namespace ConnectFourGame
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Mac!");
+
+            Console.WriteLine("Hello Windows");
         }
     }
 }
