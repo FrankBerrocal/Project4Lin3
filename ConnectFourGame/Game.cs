4f78287680eec25cb79a61ebb28def76ebc4eb81
FrankBerrocal <Francisco.berrocal@me.com>
2022-04-06 15:25:50 -0600

    Game Class
    Start new game method in Start, Select Column, and check winning moving, Horizontal and vertical logic.

--- ConnectFourGame/Game.cs
+++ ConnectFourGame/Game.cs
@@ -44,11 +44,121 @@ namespace ConnectFourGame
         }
 
         //private void StartNewGame()
+        private void StartNewGame()
+        {
+            do
+            {
+                Console.Write($"Player playerName, your turn!\n");
+                SelectColumn();
+                Console.Write("\n\n");
+                board.Display();
+                if (CheckForWinningMove('X') == true)
+                {
+                    Console.Write("\n\nPlayer X wins!\n\n");
+
+                    playSound.Win();
+
+                    break;
+                }
+                else if (CheckForWinningMove('O') == true)
+                {
+                    Console.Write("\n\nPlayer O wins!\n\n");
+
+                    playSound.Win();
+
+                    break;
+                }
+            } while (true);
+        }
 
         //private void SelectColumn()
+        private void SelectColumn()
+        {
+            do
+            {
+                Console.Write("\nChoose a column [1 to 7]: ");
+                string input = Console.ReadLine();
+                int c = Int32.Parse(input);
+
+                switch (c)
+                {
+                    case 1:
+                        board.selectedColumn = 0;
+                        break;
+                    case 2:
+                        board.selectedColumn = 1;
+                        break;
+                    case 3:
+                        board.selectedColumn = 2;
+                        break;
+                    case 4:
+                        board.selectedColumn = 3;
+                        break;
+                    case 5:
+                        board.selectedColumn = 4;
+                        break;
+                    case 6:
+                        board.selectedColumn = 5;
+                        break;
+                    case 7:
+                        board.selectedColumn = 6;
+                        break;
+                    default:
+                        board.selectedColumn = 666;
+
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("\nInvalid column selected! Try again.\n\n");
+                        Console.ResetColor();
+
+                        playSound.Wrong();
+
+                        board.Display();
+                        break;
+                }
+
+                if ((board.selectedColumn >= 0 && board.selectedColumn <= 6) && (board.boardGrid[0, board.selectedColumn] == ' '))
+                {
+                    DropDisc();
+                    Board.disc = (Board.disc == 'X') ? 'O' : 'X';
+                    break;
+                }
+
+            } while (true);
+        }
 
         //private void DropDisc()
 
-        //private bool CheckForWinningMove(char disc)
+        private bool CheckForWinningMove(char disc)
+        {
+            int ROWS = Board.Rows;
+            int COLS = Board.Columns;
+
+            // check HORIZONTAL slots for a winning move
+            for (int c = 0; c < COLS - 3; c++)
+            {
+                for (int r = 0; r < ROWS; r++)
+                {
+                    if (board.boardGrid[r, c] == disc && board.boardGrid[r, c + 1] == disc && board.boardGrid[r, c + 2] == disc && board.boardGrid[r, c + 3] == disc)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+
+            // check VERTICAL slots for a winning move
+            for (int c = 0; c < COLS; c++)
+            {
+                for (int r = 0; r < ROWS - 3; r++)
+                {
+                    if (board.boardGrid[r, c] == disc && board.boardGrid[r + 1, c] == disc && board.boardGrid[r + 2, c] == disc && board.boardGrid[r + 3, c] == disc)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+
+
     }
 }
