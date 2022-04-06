7be43c5af12dd1abb946a8c602baf4de7c1832ff
FrankBerrocal <Francisco.berrocal@me.com>
2022-04-06 15:39:50 -0600

    Update Menu.cs
    Logic of menu.

--- ConnectFourGame/Menu.cs
+++ ConnectFourGame/Menu.cs
@@ -9,5 +9,51 @@ namespace ConnectFourGame
     public class Menu
     {
         // Main Menu here
+        
+        private int SelectedIndex;
+        private string[] Options;
+        public static string[] options = new string[] { "Play Game", "Change Language", "Exit Game" };
+
+        public Menu(string[] options)
+        {
+            Options = options;
+            SelectedIndex = 0;
+        }
+        
+
+        public int Run()
+        {
+            ConsoleKey keyPressed;
+
+            do
+            {
+                Console.Clear();
+                //DisplayOptions();
+
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                keyPressed = keyInfo.Key;
+
+                // Update SelectedIndex based on arrow keys.
+                if (keyPressed == ConsoleKey.UpArrow)
+                {
+                    SelectedIndex--;
+                    if (SelectedIndex == -1)
+                    {
+                        SelectedIndex = 0;
+                    }
+                }
+                else if (keyPressed == ConsoleKey.DownArrow)
+                {
+                    SelectedIndex++;
+                    if (SelectedIndex == Options.Length)
+                    {
+                        SelectedIndex = Options.Length - 1;
+                    }
+                }
+
+            } while (keyPressed != ConsoleKey.Enter);
+
+            return SelectedIndex;
+        }
     }
 }
