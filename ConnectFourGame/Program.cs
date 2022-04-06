b1b716f3accc041bca6775ada47793429036e082
Peter <p.kosa089@mybvc.ca>
2022-04-06 14:02:27 -0600

    Commit Game class

--- ConnectFourGame/Program.cs
+++ ConnectFourGame/Program.cs
@@ -6,7 +6,7 @@ namespace ConnectFourGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Mac!");
         }
     }
 }
