1f6e92764a34a32ace0f284b32790214eec9d371
Peter <p.kosa089@mybvc.ca>
2022-04-06 13:54:29 -0600

    Created new .NET 5 project to work on both Windows and MAC

---
+++ ConnectFourGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConnectFourGame
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-# Project4Lin3
