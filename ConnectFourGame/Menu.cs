f28a2eb07dacfcfcbadce2e023683fa900146da8
Peter <p.kosa089@mybvc.ca>
2022-04-06 15:44:39 -0600

    Commit DisplayOptions method to Menu class that contains menu graphic and buttons

--- ConnectFourGame/Menu.cs
+++ ConnectFourGame/Menu.cs
@@ -19,7 +19,41 @@ namespace ConnectFourGame
             Options = options;
             SelectedIndex = 0;
         }
-        
+
+
+        private void DisplayOptions()
+        {
+            string title = @"   ______                            __     ______                    ______                   
+  / ____/___  ____  ____  ___  _____/ /_   / ____/___  __  _______   / ____/___ _____ ___  ___ 
+ / /   / __ \/ __ \/ __ \/ _ \/ ___/ __/  / /_  / __ \/ / / / ___/  / / __/ __ `/ __ `__ \/ _ \
+/ /___/ /_/ / / / / / / /  __/ /__/ /_   / __/ / /_/ / /_/ / /     / /_/ / /_/ / / / / / /  __/
+\____/\____/_/ /_/_/ /_/\___/\___/\__/  /_/    \____/\__,_/_/      \____/\__,_/_/ /_/ /_/\___/                                                                                                                                                                                                                                 
+";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(title);
+            Console.ResetColor();
+
+            for (int i = 0; i < Options.Length; i++)
+            {
+                string currentOption = Options[i];
+
+                if (i == SelectedIndex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+
+                Console.WriteLine($"<< {currentOption} >>");
+
+                Console.ResetColor();
+            }
+        }
+
 
         public int Run()
         {
@@ -28,7 +62,7 @@ namespace ConnectFourGame
             do
             {
                 Console.Clear();
-                //DisplayOptions();
+                DisplayOptions();
 
                 ConsoleKeyInfo keyInfo = Console.ReadKey(true);
                 keyPressed = keyInfo.Key;
