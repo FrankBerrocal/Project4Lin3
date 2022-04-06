78d276aaf59238a02ad367d383c940b8cd534283
Peter <p.kosa089@mybvc.ca>
2022-04-06 15:08:59 -0600

    Display Board class and DisplayMainMenu method

--- ConnectFourGame/Board.cs
+++ ConnectFourGame/Board.cs
@@ -9,5 +9,59 @@ namespace ConnectFourGame
     public class Board
     {
         // This is a class for our board.
+        // number of rows in the board
+        public static int Rows { get; private set; } = 6;
+
+        // number of columns in the board
+        public static int Columns { get; private set; } = 7;
+
+        // 6 by 7 character array representing the game board
+
+        public char[,] boardGrid = new char[Rows, Columns];
+
+        // disc
+        public static char disc = 'X';
+
+        //public static int selectedColumn = 0;
+        public int selectedColumn;
+
+        // fill game board array with empty characters
+        public void Initiate()
+        {
+            // r represents rows while c represents columns in the array
+
+            for (int r = 0; r < Rows; r++)
+            {
+                for (int c = 0; c < Columns; c++)
+                {
+                    boardGrid[r, c] = ' ';
+                }
+            }
+        }
+
+
+        // print the board at its current state
+        public void Display()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("  1   2   3   4   5   6   7  ");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine("|---|---|---|---|---|---|---|");
+
+            int i, j;
+
+            for (i = 0; i < Rows; i++)
+            {
+                for (j = 0; j < Columns; j++)
+                {
+                    Console.Write($"| {boardGrid[i, j]} ");
+                }
+                Console.WriteLine("|");
+                Console.WriteLine("|---|---|---|---|---|---|---|");
+            }
+        }
     }
 }
--- ConnectFourGame/Game.cs
+++ ConnectFourGame/Game.cs
@@ -9,14 +9,39 @@ namespace ConnectFourGame
     public class Game
     {
         // Main game logic here
+        Board board = new Board();
 
         public void Run()
         {
-            //DisplayMainMenu();
+            DisplayMainMenu();
             Console.ReadLine();
         }
 
-        //private void DisplayMainMenu()
+        private void DisplayMainMenu()
+        {
+            Menu menu = new Menu(Menu.options);
+            int selectedIndex = menu.Run();
+
+            switch (selectedIndex)
+            {
+                case 0:
+                    Console.Clear();
+                    board.Initiate();
+                    board.Display();
+                    StartNewGame();
+                    break;
+                case 1:
+                    // Change language
+
+                    break;
+                case 2:
+                    // Exit game
+                    Console.WriteLine("\nPress Enter to exit");
+                    Console.ReadKey(true);
+                    Environment.Exit(0);
+                    break;
+            }
+        }
 
         //private void StartNewGame()
 
