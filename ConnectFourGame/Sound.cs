598b7de987fade81c17e24253d6d8c62dc3b91ae
Peter <p.kosa089@mybvc.ca>
2022-04-09 12:36:13 -0600

    Minor changes to the structure

--- ConnectFourGame/Sound.cs
+++ ConnectFourGame/Sound.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Media;
 
 namespace ConnectFourGame
 {
     public class Sound
     {
-        // Sound effects here
+        // Sound effects here.
     }
 }
